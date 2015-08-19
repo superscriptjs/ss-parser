b73d92100abd29018623ea8762d34e8a08e7d4b1
Rob Ellis <rob@chloi.io>
2015-08-18 18:40:45 -0700

    Actually test something

--- test/main.js
+++ test/main.js
@@ -4,11 +4,19 @@ var should = require("should");
 var parse = require("../lib/")();
 
 describe('Should Parse Input', function() {
-  it("It should work", function (done) {
-
+  it("Should be an object", function (done) {
     parse.loadDirectory('./test/fixtures/main.ss', function(err, result) {
-      console.log(err, result);
+      // Should have the following keys
+      ['topics', 'gambits', 'replys', 'checksums'].should.eql(Object.keys(result));
+
+      // We should have 2 topics
+      ['random', 'random2'].should.eql(Object.keys(result.topics));
+
+      // We should have some gambits
+      Object.keys(result.gambits).should.have.length(27);
+      Object.keys(result.replys).should.have.length(28);
       done();
+      
     });
   });
 });
