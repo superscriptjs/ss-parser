c8f1d67e7e6f5da7909d8663750327abeed08354
Rob Ellis <rob@chloi.io>
2015-10-12 10:34:33 -0700

    fix race condition in parsecontents

--- test/main.js
+++ test/main.js
@@ -16,6 +16,10 @@ describe('Should Parse Input', function() {
       // We should have some gambits
       Object.keys(result.gambits).should.have.length(27);
       Object.keys(result.replys).should.have.length(28);
+
+      // Lets make sure we have a conversations array as well
+      var key = Object.keys(result.gambits).pop();
+      result.gambits[key].options.conversations.should.have.length(3);
       done();
       
     });
