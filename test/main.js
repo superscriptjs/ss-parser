d58b277f5b0953dcf2ef819f33f36d1ddd57ae8b
Rob Ellis <rellis@slack-corp.com>
2016-01-18 14:13:52 -0800

    updated test

--- test/main.js
+++ test/main.js
@@ -17,14 +17,14 @@ describe('Should Parse Input', function() {
     parse.loadDirectory('./test/fixtures/main.ss', function(err, result) {
 
       // Should have the following keys
-      ['topics', 'gambits', 'replys', 'checksums'].should.eql(Object.keys(result));
+      ['topics', 'gambits', 'convos', 'replys', 'checksums'].should.eql(Object.keys(result));
 
       // We should have 4 topics
       ['random', '__pre__', '__post__', 'random2'].should.eql(Object.keys(result.topics));
 
       // We should have some gambits
-      Object.keys(result.gambits).should.have.length(29);
-      Object.keys(result.replys).should.have.length(30);
+      Object.keys(result.gambits).should.have.length(35);
+      Object.keys(result.replys).should.have.length(36);
 
       // Lets make sure we have a conversations array as well
       var key = Object.keys(result.gambits).pop();
